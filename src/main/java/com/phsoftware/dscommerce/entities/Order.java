dea3610b884326f19547bcb7cc65212fd248f76e
paschoalhenrique <paschoalhenrique@hotmail.com>
2026-07-14 10:52:36 -0300

    Criando Payment construtores e atributos.

--- src/main/java/com/phsoftware/dscommerce/entities/Order.java
+++ src/main/java/com/phsoftware/dscommerce/entities/Order.java
@@ -12,7 +12,6 @@ public class Order {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private Long id;
-  //@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant moment;
     private OrderStatus status;
@@ -21,6 +20,9 @@ public class Order {
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
 
     public Order(){
     }

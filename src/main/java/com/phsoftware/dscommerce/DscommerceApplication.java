f0fd1288ebb3d385a81e9867358ce454c177dcb1
paschoalhenrique <paschoalhenrique@hotmail.com>
2026-07-28 15:10:22 -0300

    Atualização projeto.

--- src/main/java/com/phsoftware/dscommerce/DscommerceApplication.java
+++ src/main/java/com/phsoftware/dscommerce/DscommerceApplication.java
@@ -1,14 +1,19 @@
 package com.phsoftware.dscommerce;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DscommerceApplication {
+public class DscommerceApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DscommerceApplication.class, args);
-		System.out.println("Theózinho");
+
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Theózinho deu certo ");
+	}
 }

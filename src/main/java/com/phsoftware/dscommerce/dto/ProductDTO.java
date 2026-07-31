398337633eae96af65bd9fda0ae0931782376de7
paschoalhenrique <paschoalhenrique@hotmail.com>
2026-07-31 17:39:46 -0300

    Validação de dados com Bean Validation.

--- src/main/java/com/phsoftware/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/phsoftware/dscommerce/dto/ProductDTO.java
@@ -1,16 +1,39 @@
-package com.phsoftware.dscommerce.dto;
+package com.devsuperior.dscommerce.dto;
 
-import com.phsoftware.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.entities.Category;
+import com.devsuperior.dscommerce.entities.Product;
+import jakarta.validation.constraints.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <h1> DEVSUPERIOR - Java Spring Professional - Capítulo: Login e controle de acesso</h1>
+ * DESAFIO: Projeto Spring Boot estruturado
+ * <p>
+ * <b>Note:</b> Desenvolvido na linguagem Java.
+ *
+ * @author  Marcos Ferreira Shirafuchi
+ * @version 1.0
+ * @since   22/07/2024
+ */
 
 public class ProductDTO {
     private Long id;
+    @Size(min = 3, max = 80, message = "Nome precisa ter 3 a 80 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String name;
+    @Size(min = 10, message = "Descrição precisa ter no mínimo 10 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String description;
+    @NotNull(message = "Campo requerido")
+    @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
 
-    public ProductDTO() {
-    }
+    //Lista de categoryDTO
+    @NotEmpty(message = "Deve ter pelo menos uma categoria")
+    private List<CategoryDTO> categories = new ArrayList<>();
+
 
     public ProductDTO(Long id, String name, String description, Double price, String imgUrl) {
         this.id = id;
@@ -20,14 +43,18 @@ public class ProductDTO {
         this.imgUrl = imgUrl;
     }
 
-    // Construtor que copia os dados da entidade Product
-    public ProductDTO(Product product) {
-        this.id = product.getId();
-        this.name = product.getName();
-        this.description = product.getDescription();
-        this.price = product.getPrice();
-        this.imgUrl = product.getImgUrl();
+    public ProductDTO(Product entity) {
+        id = entity.getId();
+        name = entity.getName();
+        description = entity.getDescription();
+        price = entity.getPrice();
+        imgUrl = entity.getImgUrl();
+        for (Category cat : entity.getCategories()){
+            categories.add(new CategoryDTO(cat));
+        }
     }
+
+    //O DTO não altera, só tem os getters
 
     public Long getId() {
         return id;
@@ -48,4 +75,9 @@ public class ProductDTO {
     public String getImgUrl() {
         return imgUrl;
     }
+
+    public List<CategoryDTO> getCategories() {
+        return categories;
+    }
+    //corrigir classe DTO mais tarde
 }
